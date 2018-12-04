fee8f4fdd1f5fbbb18cc41223f2ad0cb518e5710
Daniel Steven Ocampo <danielsteven23@outlook.com>
2018-12-04 02:43:11 -0500

    Tercer Commit

--- src/main/java/CalculatorMaven/CalculatorMaven/App.java
+++ src/main/java/CalculatorMaven/CalculatorMaven/App.java
@@ -1,13 +1,130 @@
 package CalculatorMaven.CalculatorMaven;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Calculadora ICESI");
+		BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+		boolean continuar = true;
+		Calculadora calculadora = new Calculadora();
+		do {
+			try {
+
+				System.out.println("Que operación desea realizar?");
+				System.out.println("1. Suma");
+				System.out.println("2. Resta");
+				System.out.println("3. Multiplicación");
+				System.out.println("4. División");
+				System.out.println("5. Modulo");
+				System.out.println("6. Exponente");
+				System.out.println("7. Factorial");
+				System.out.println("8. Es Primo");
+				System.out.println("9. Es Par");
+				System.out.println("10. Es Impar");
+				System.out.println("11. Salir");
+
+				int opcion = Integer.parseInt(lector.readLine());
+				switch (opcion) {
+				case 1:
+					System.out.println("Digite el primero número a sumar");
+					int a1 = Integer.parseInt(lector.readLine());
+					System.out.println("Digite el segundo número a sumar");
+					int b1 = Integer.parseInt(lector.readLine());
+					
+					long resultado1 = calculadora.suma(a1, b1);
+					System.out.println("Resultado: "+resultado1);
+					break;					
+				case 2:
+					System.out.println("Digite el minuendo");
+					int a2 = Integer.parseInt(lector.readLine());
+					System.out.println("Digite el sustraendo");
+					int b2 = Integer.parseInt(lector.readLine());
+					
+					int resultado2 = calculadora.resta(a2, b2);
+					System.out.println("Resultado: "+resultado2);
+					break;
+				case 3:
+					System.out.println("Digite el primer número a multiplicar");
+					int a3 = Integer.parseInt(lector.readLine());
+					System.out.println("Digite el segundo número a multiplicar");
+					int b3 = Integer.parseInt(lector.readLine());
+					
+					long resultado3 = calculadora.multiplicacion(a3, b3);
+					System.out.println("Resultado: "+resultado3);
+
+					break;
+				case 4:
+					System.out.println("Digite el dividendo");
+					int a4 = Integer.parseInt(lector.readLine());
+					System.out.println("Digite el divisor");
+					int b4 = Integer.parseInt(lector.readLine());
+					
+					double resultado4 = calculadora.division(a4, b4);
+					System.out.println("Resultado: "+resultado4);
+					break;
+				case 5:
+					System.out.println("Digite el dividendo");
+					int a5 = Integer.parseInt(lector.readLine());
+					System.out.println("Digite el divisor");
+					int b5 = Integer.parseInt(lector.readLine());
+					
+					int resultado5 = calculadora.modulo(a5, b5);
+					System.out.println("Resultado: "+resultado5);
+					break;
+				case 6:
+					System.out.println("Digite la base");
+					int a6 = Integer.parseInt(lector.readLine());
+					System.out.println("Digite el exponente");
+					int b6 = Integer.parseInt(lector.readLine());
+					
+					long resultado6 = calculadora.exponente(a6, b6);
+					System.out.println("Resultado: "+resultado6);
+					break;
+				case 7:
+					System.out.println("Digite número a calcular factorial");
+					int a7 = Integer.parseInt(lector.readLine());
+					long resultado7 = calculadora.factorial(a7);
+					System.out.println("Resultado: "+resultado7);
+					break;
+				case 8:
+					System.out.println("Digite número para validar si es primo");
+					int a8 = Integer.parseInt(lector.readLine());
+					boolean resultado8 = calculadora.esPrimo(a8);
+					System.out.println("Resultado: "+resultado8);
+					break;
+				case 9:
+					System.out.println("Digite número para validar si es par");
+					int a9 = Integer.parseInt(lector.readLine());
+					boolean resultado9 = calculadora.esPar(a9);
+					System.out.println("Resultado: "+resultado9);
+					break;
+				case 10:
+					System.out.println("Digite número para validar si es impar");
+					int a10 = Integer.parseInt(lector.readLine());
+					boolean resultado10 = calculadora.esImpar(a10);
+					System.out.println("Resultado: "+resultado10);
+					break;
+				case 11:
+					continuar = false;
+					break;
+
+				default:
+					System.out.println("Por favor digite una opción valida. Un número entre 1 y 11");
+					break;
+				}
+
+			} catch (NumberFormatException e) {
+				System.out.println("Por favor digite un número entero");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		} while (continuar);
+
+		System.out.println("Bye");
+
+	}
 }
---
+++ src/main/java/CalculatorMaven/CalculatorMaven/Calculadora.java
@@ -0,0 +1,45 @@
+package CalculatorMaven.CalculatorMaven;
+
+public class Calculadora {
+	
+	
+	public long suma(int a, int b) {
+		return 0;
+	}
+
+	public int resta(int a, int b) {
+		return 0;
+	}
+	
+	public long multiplicacion(int a, int b) {
+		return 0;
+	}
+	
+	public double division(int a, int b) {
+		return 0;
+	}
+	
+	public int modulo(int a, int b) {
+		return 0;
+	}
+	
+	public long exponente(int a, int b) {
+		return 0;
+	}
+	
+	public long factorial(int a) {
+		return 0;
+	}
+	
+	public boolean esPrimo(int a) {
+		return false;
+	}
+	
+	public boolean esPar(int a) {
+		return false;
+	}
+	
+	public boolean esImpar(int a) {
+		return false;
+	}
+}
