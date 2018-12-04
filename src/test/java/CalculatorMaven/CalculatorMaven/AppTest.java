9ec648e0ace5b4ed074390d3d55cd4cc6d57202d
Daniel Steven Ocampo <danielsteven23@outlook.com>
2018-12-04 04:17:40 -0500

    cambios junit

--- src/test/java/CalculatorMaven/CalculatorMaven/AppTest.java
+++ src/test/java/CalculatorMaven/CalculatorMaven/AppTest.java
@@ -1,9 +1,7 @@
 package CalculatorMaven.CalculatorMaven;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
 
 public class AppTest {
 
