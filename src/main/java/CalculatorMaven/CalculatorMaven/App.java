a754d5d710e871721a09272ab855abf47f92ffaf
Daniel Steven Ocampo <danielsteven23@outlook.com>
2018-12-03 23:24:01 -0500

    my first commit

---
+++ src/main/java/CalculatorMaven/CalculatorMaven/App.java
@@ -0,0 +1,13 @@
+package CalculatorMaven.CalculatorMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
